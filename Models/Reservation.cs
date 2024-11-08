32441feb8dd2770d4b2f13a052162892a6db9d22
Jerald2077 <jheraldo.soto@gmail.com>
2024-11-08 15:35:52 -0300

    Agregacion de RoomDTO.cs y Reservation

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -1,9 +1,14 @@
+using System.Text.Json.Serialization;
+
 public class Reservation
 {
     public int Id { get; set; }                      // Identificador único de la reserva
-    public int RoomId { get; set; }                  // Id de la habitación reservada
+    public int RoomId { get; set; }// Id de la habitación reservada
+
+    [JsonIgnore]
     public Room? Room { get; set; }                   // Relación con el modelo Room
-    public string? UserId { get; set; }               // Id del usuario que realizó la reserva
+    public string? UserId { get; set; }              // Id del usuario que realizó la reserva
+    [JsonIgnore]
     public ApplicationUser? User { get; set; }        // Relación con el modelo ApplicationUser
 
     public DateTime StartDate { get; set; }          // Fecha de inicio de la reserva
